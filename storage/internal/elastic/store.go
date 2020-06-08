38da6beaf2b6adb149f75ea7d8fa6f9ee8ed8866
Xavier DEGENNE <xavier.degenne@gmail.com>
2020-06-08 21:26:44 +0200

    WIP

--- storage/internal/elastic/store.go
+++ storage/internal/elastic/store.go
@@ -485,7 +485,7 @@ func (s *elasticStore) SetCollection(ctx context.Context, keyValues []store.KeyV
 
 			// The bulk action
 			index := `{"index":{"_index":"` + s.getIndexName(storeType) + `","_type":"logs_or_event"}}`
-			bulkOperation := make([]byte, len(index) + len(document) + 4)
+			bulkOperation := make([]byte, 0)
 			bulkOperation = append(bulkOperation, index...)
 			bulkOperation = append(bulkOperation, "\n"...)
 			bulkOperation = append(bulkOperation, document...)
